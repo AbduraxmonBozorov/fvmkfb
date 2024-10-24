2d025aaad2d7c83717597c16051008d72f3cd4a0
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-10-24 09:43:34 +0500

    header-user section add log out btn

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function DropDown() {
+
+  function handleLoginOut(){
+    localStorage.removeItem("authToken");
+  }
+
   return (
-    <div>
-        <Link to="">Profile</Link>
+    <div className='border flex flex-col bg-white'>
+        <Link className='p-2 border hover:bg-slate-100' to="">Profile</Link>
+        <Link className='p-2 border hover:bg-slate-100' to="/login" onClick={handleLoginOut}>Log out</Link>
     </div>
   )
 }
